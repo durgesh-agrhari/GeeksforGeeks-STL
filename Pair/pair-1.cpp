96a4c642391eb457481745bc5b0e550a10d82da9
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-06-30 16:15:43 +0530

    Create pair-1.cpp

---
+++ Pair/pair-1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cin>>n;
+    int vrr[n];
+    for (int i=0; i<n; i++){
+        cin>>vrr[i];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<vrr[i]<<" ";
+    }
+  return 0;
+
+}
