f18e8e79f32727d9fecb2fd573c494416f9a088c
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-06-30 15:59:36 +0530

    Create pair1.cpp

---
+++ Pair/pair1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+//#include <vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int a,b;
+        cin>>a>>b;
+        pair<int,int> p1(a,b);
+        cout<<"{"<<p1.first<<","<<p1.second<<"}"<<endl;
+        
+    }
+}
